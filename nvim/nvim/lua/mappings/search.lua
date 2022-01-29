238d808b4a369c0551031ab6f57492814d496161
Jared Mackey <jared@mackey.tech>
2022-01-29 13:10:36 -0700

    Fix telescope fb to open in current directory

--- nvim/nvim/lua/mappings/search.lua
+++ nvim/nvim/lua/mappings/search.lua
@@ -19,4 +19,4 @@ telescope.load_extension "file_browser"
 map('n', '<leader>ff', ":Telescope find_files find_command=fd,--type,file,--hidden,-g,-E,.git/<cr>", default_opts)
 map('n', '<leader>fr', ":Telescope buffers<cr>", default_opts)
 map('n', '<leader>fg', ":Telescope git_status<cr>", default_opts)
-map('n', '<leader>fb', ":Telescope file_browser<cr>", default_opts)
+map('n', '<leader>fb', ":Telescope file_browser path=%:p:h<cr>", default_opts)
