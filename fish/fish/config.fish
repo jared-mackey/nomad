8180b0e6a7221067a606c0c31ab9a11d5ce22606
Jared Mackey <jared@mackey.tech>
2026-09-01 10:50:46 -0600

    various updates

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -9,7 +9,7 @@ fish_add_path -a -g ~/.local/bin
 ################################################################################
 # TERMINAL
 ################################################################################
-set -gx GPG_TTY $(tty)
+set -gx GPG_TTY (tty)
 set -gx EDITOR nvim
 set -gx MANPAGER nvim +Man!
 
@@ -20,7 +20,17 @@ alias kctx="kubectx"
 alias kns="kubens"
 alias k="kubectl"
 alias kgp="kubectl get pods"
+alias tshproxykube="tsh proxy kube -p 8833 &"
 alias consul-forward="kubectl port-forward -n kv consul-server-0 8500:8500 &; sleep 2; xdg-open http://localhost:8500; fg"
+set -xg KUBECONFIG /home/jared/.tsh/keys/teleport.yendostaging.com/jared-kube/teleport.yendostaging.com/localproxy-8833-kubeconfig
+set -xg TELEPORT_ADD_KEYS_TO_AGENT no
+
+################################################################################
+# Python
+################################################################################
+set -Ux PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv init - | source
 
 ################################################################################
 # Elixir
@@ -48,6 +58,16 @@ fish_add_path -a -g $ANDROID_HOME/platform-tools/
 alias nomad='sudo salt-call state.apply'
 
 ################################################################################
+# Flutter / Dart
+################################################################################
+fish_add_path -a -g $HOME/dev/yendo/flutter/bin
+export ANDROID_HOME="$HOME/Android"   # this might be different for you, or android studio does it automatically. Depends!
+export ANDROID_SDK_ROOT=$ANDROID_HOME/Sdk
+fish_add_path -a -g $ANDROID_SDK_ROOT/emulator
+fish_add_path -a -g $ANDROID_SDK_ROOT/platform-tools
+fish_add_path -a -g $ANDROID_SDK_ROOT/cmdline-tools/latest/bin
+
+################################################################################
 # RUST
 ################################################################################
 fish_add_path -a -g ~/.cargo/bin
@@ -55,12 +75,26 @@ fish_add_path -a -g ~/.cargo/bin
 ################################################################################
 # Go
 ################################################################################
-export GOBIN="$HOME/.local/bin/"
+set -xg GOBIN "$HOME/.local/bin/"
+# set GO max procs to number of cores - 20% to avoid overloading the system
+set -xg GOMAXPROCS (nproc | awk '{print int($1 * 0.8)}')
+set -xg GOMEMLIMIT '2GiB'
+# move go cache to a ramdisk 
+# Set   GOFLAGS to use 25% of available cores for parallel builds, to avoid overloading the system
+set -xg GOFLAGS (nproc | awk '{print "-p=" int($1 * 0.25)}')
+alias go-lint='golangci-lint run --new-from-rev (git rev-parse origin/staging)'
+fish_add_path -a -g $HOME/.rover/bin
+
+################################################################################
+# Docker aliases
+################################################################################
+alias docker-compose="docker compose"
+
 ################################################################################
 # ASDF
 ################################################################################
-source $XDG_CODE_HOME/asdf/asdf.fish
-source $XDG_CODE_HOME/asdf/completions/asdf.fish
+set -xg ASDF_DATA_DIR "$HOME/.asdf"
+fish_add_path -p -g $ASDF_DATA_DIR/shims
 
 ################################################################################
 # Rust alternatives
@@ -77,7 +111,7 @@ zoxide init fish | source
 ################################################################################
 # Direnv
 ################################################################################
-eval "$(direnv hook fish)"
+direnv hook fish | source
 
 
 ################################################################################
@@ -91,3 +125,5 @@ end
 # Starship
 ################################################################################
 starship init fish | source
+
+remuda completions fish | source
